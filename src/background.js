b3fcd2ba7b80114fd684b1c6abd40264c7ed1706
Luciano Júnior <luciannojunior@gmail.com>
2019-10-25 18:45:16 -0300

    Melhorando background.js

--- src/background.js
+++ src/background.js
@@ -16,39 +16,37 @@ const injectCSS = (path, tabId) =>
     )}'; document.head.appendChild(link);}`
   })
 const getCurrentPage = url => {
+  if (url.includes('notas')) return PAGES.NOTAS
+  if (url.includes('frequencia')) return PAGES.FREQUENCIA
   if (!url || !url.split('?')) return null
   const query = url.split('?').pop()
   const params = new URLSearchParams(query)
   return params.get('command')
 }
 
+const PAGES = {
+  FREQUENCIA: 'frequencia',
+  NOTAS: 'notas'
+}
+
+const injectorsMap = {
+  [PAGES.FREQUENCIA]: 'injectors/frequencia.js',
+  [PAGES.NOTAS]: 'injectors/notas.js'
+}
+
+const WHITELIST = ['localhost', 'ufcgexamples', 'pre.ufcg.edu.br:8443/ControleAcademicoOnline/']
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (~tab.url.indexOf('localhost') || ~tab.url.indexOf('pre.ufcg.edu.br:8443/ControleAcademicoOnline/')) {
+  const { url } = tab
+  const shouldAct = WHITELIST.reduce((acc, cur) => acc || url.includes(cur), false) 
+  if (!shouldAct) return
     chrome.pageAction.show(tabId)
 
     injectCSS('ufcg_tachyons.css')
     injectScript('util.js')
     injectScript('https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js', tabId, true)
-    // injectScript('popup/popup.js')
 
-    //TESTES
-    if (tab.url.includes('frequencia')) {
-      console.log('Injetor de frequência')
-      injectScript('injectors/frequencia.js')
-      // injectScript('home.js')
-    } else if (tab.url.includes('notas')) {
-      console.log('Injetor de notas')
-      injectScript('injectors/notas.js')
-    }
-    //ENDTESTES
-    // switch (getCurrentPage(tab.url)) {
-    //   case 'Home':
-    //   case '':
-    //   case null:
-    //     injectScript('home.js')
-    //     break
-    //   default:
-    //     break
-    // }
-  }
+    const page = getCurrentPage(url)
+    const shouldInject = injectorsMap[page]
+    shouldInject && injectScript(shouldInject)
+    
 })
