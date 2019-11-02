6e6c1506a98241a453d0feede3130c4896670270
Luciano Júnior <luciannojunior@gmail.com>
2019-11-02 01:01:41 -0300

    Funcionalidade de verificação de problema nas disciplinas selecionadas

--- src/components/Planner.vue
+++ src/components/Planner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row justify-around bgb ph3 pa2 br4" style="height: 260px">
+  <div class="flex flex-row justify-around bgb ph3 pa2 br4" style="min-height: 260px">
     <div class="w-50">
       <h3 class="text-center">Planeje sua matrícula</h3>
       <p>Selecione na tabela as disciplinas que você planeja se matricular. Quando a matrícula for liberada, <b>você poderá selecionar as disciplinas com apenas um clique.</b></p>
@@ -38,9 +38,23 @@
 </template>
 
 <script>
-// TODO
-const verificarProblema = (cadeiras, cadeira) => {
-  return null
+const getCadeirasCursadas = async () => {
+  const doc = await ufcg.fetchAndParse('/ControleAcademicoOnline/Controlador?command=AlunoHistorico')
+  const mapper = {
+    'Carga horária': 'ch',
+    Créditos: 'cr',
+    Código: 'id',
+    Disciplina: 'nome',
+    Período: 'periodo',
+    Situação: 'situacao',
+    Tipo: 'tipo',
+    Média: 'nota'
+  }
+
+  const config = {
+    mapHeader: h => mapper[h]
+  }
+  return ufcg.table2json(doc.querySelector('table'), config)
 }
 
 const ITEM_NAME = 'matricula'
@@ -57,8 +71,7 @@ export default {
           }
         })
       } else {
-        // TODO
-        cods = []
+        const cods = this.cursadas.map(({ id }) => id)
         document.querySelectorAll('tbody tr').forEach(tr => {
           if (!tr.children[4]) return
           const cod = tr.children[1].innerText.split('-')[0].trim()
@@ -94,21 +107,42 @@ export default {
         }
         return 0
       })
+    },
+    verificarProblema(cadeiras, cadeira) {
+      if (
+        this.cursadas
+          .filter(({ situacao }) => ['Aprovado', 'Dispensa'].includes(situacao))
+          .map(({ id }) => id)
+          .includes(cadeira.codigo.split('-')[0].trim())
+      ) {
+        return 'Você já cursou essa cadeira'
+      }
+      const slots = cadeira.horario.split('\n').map(word => word.slice(0, 4))
+      const [match] = cadeiras.filter(({ horario }) => {
+        return slots.some(slot => horario.includes(slot))
+      })
+      if (match) {
+        return `Essa cadeira tem conflito de horário com ${match.nome}`
+      }
     }
   },
   data: function() {
     return {
-      disciplinasMarcadas: null
+      disciplinasMarcadas: null,
+      cursadas: null
     }
   },
   mounted() {
+    getCadeirasCursadas().then(cadeiras => {
+      this.cursadas = cadeiras
+    })
     this.atualizarState()
     document.addEventListener('selectCadeira', ({ detail }) => {
       const { checked, ...cadeira } = detail
       const cadeirasDB = JSON.parse(localStorage.getItem(ITEM_NAME)) || []
       const updated = checked ? [...cadeirasDB, cadeira] : cadeirasDB.filter(({ codigo }) => codigo !== cadeira.codigo)
-      const problema = verificarProblema(cadeirasDB, cadeira)
-      problema && alert(problema)
+      const problema = this.verificarProblema(cadeirasDB, cadeira)
+      checked && problema && alert(problema)
       localStorage.setItem(ITEM_NAME, JSON.stringify(updated))
       this.atualizarState()
     })
